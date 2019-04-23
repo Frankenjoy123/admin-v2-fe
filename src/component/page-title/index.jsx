074ca20c9e1cfd0f17dd211daf6e2eb6e2a96f61
xiaowu.zhou <xiaowu.zhou@tongdun.cn>
2019-04-23 22:01:12 +0800

    通用部分的开发

---
+++ src/component/page-title/index.jsx
@@ -0,0 +1,25 @@
+
+import React , {Component} from 'react'
+
+export default class PageTitle extends Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    componentWillMount(){
+        document.title = this.props.title + '-HAPPY MMALL';
+    }
+
+    render(){
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className={"page-header"}>{this.props.title}</h1>
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+
+}
--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,11 +1,20 @@
 
 import React from 'react';
+import PageTitle from 'component/page-title/index.jsx';
 
 export  default  class Home extends React.Component {
 
     render(){
         return (
           <div id="page-wrapper">
+              <PageTitle title="首页">
+                  <button className="btn btn-waring"></button>
+              </PageTitle>
+              <div className="row">
+                  <div className="col-md-12">
+                      body
+                  </div>
+              </div>
               <button className="btn btn-default">test</button>
           </div>
         );
